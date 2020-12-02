912c412b0e650eb929048413d6f29f50befa876e
sasho17c1064 <75291987+sasho17c1064@users.noreply.github.com>
2020-12-02 12:29:18 +0900

    Update .myled.mod.cmd

--- .myled.mod.cmd
+++ .myled.mod.cmd
@@ -1,1 +1,4 @@
+//SPDX-License-Identifier: GPL-3.0
+//Copyright (C) 2020 Ryuichi Ueda & Tatsuya Sasho.
+
 cmd_/home/ubuntu/robosys_device_drivers/myled.mod := { echo  /home/ubuntu/robosys_device_drivers/myled.o; echo; } > /home/ubuntu/robosys_device_drivers/myled.mod
